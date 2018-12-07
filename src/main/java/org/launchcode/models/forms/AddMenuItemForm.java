578dc9f99728ac7b84bbc92c6fbc572a0cca08d8
Angela Mitchell <anglarenmit@gmail.com>
2018-12-06 23:22:14 -0600

    Completed Draft of Cheese MVC Persistent

---
+++ src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
@@ -0,0 +1,53 @@
+package org.launchcode.models.forms;
+
+import org.launchcode.models.Cheese;
+import org.launchcode.models.Menu;
+
+import javax.validation.constraints.NotNull;
+
+public class AddMenuItemForm {
+
+    @NotNull
+    private int menuId;
+
+    @NotNull
+    private int cheeseId;
+
+    private Menu menu;
+
+    private Iterable<Cheese> cheeses;
+
+    public AddMenuItemForm() {
+
+    }
+
+    public AddMenuItemForm(Menu menu, Iterable<Cheese> cheeses) {
+        this.menu = menu;
+        this.cheeses = cheeses;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public Iterable<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public int getMenuId() {
+        return menuId;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
+    }
+
+}
